47a47d39fa343a8efd9c74d78ebd43cb99d99440
Michal Nowicki <michalsminowicki@gmail.com>
2014-12-11 14:34:39 +0100

    Added simple detection

---
+++ src/Matcher/matcher.cpp
@@ -0,0 +1,14 @@
+/** @file matcher.cpp
+ *
+ * implementation -
+ *
+ */
+#include "../include/Matcher/matcher.h"
+
+using namespace putslam;
+
+bool Matcher::match(const SensorFrame& next_frame)
+{
+
+	return false;
+}
