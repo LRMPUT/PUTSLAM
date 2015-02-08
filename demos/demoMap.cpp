d531a4780b950031b566e0db9f7e4509c93b7494
Michal Nowicki <michalsminowicki@gmail.com>
2015-02-08 22:51:43 +0100

    Adding the methods needed by map to matching

--- demos/demoMap.cpp
+++ demos/demoMap.cpp
@@ -23,7 +23,7 @@ int main(int argc, char * argv[])
 
         // create kabsch transform estimator
         Map* map = createFeaturesMap(configFileMap, configFileGrabber);
-        map->startOptimizationThread(1);
+       // map->startOptimizationThread(1);
 
         for (int i=0;i<10;i++){
             //add some data to the map
--- src/Matcher/matcher.cpp
+++ src/Matcher/matcher.cpp
@@ -30,8 +30,17 @@ void Matcher::loadInitFeatures(const SensorFrame &sensorData)
 	prevFeatures3D = RGBD::keypoints2Dto3D(prevFeatures, sensorData.depthImage);
 
 	// Save rgb/depth images
-	prevRgbImage = sensorData.rgbImage.clone();
-	prevDepthImage = sensorData.depthImage.clone();
+	prevRgbImage = sensorData.rgbImage;
+	prevDepthImage = sensorData.depthImage;
+}
+
+Matcher::featureSet Matcher::getFeatures()
+{
+	featureSet returnSet;
+	returnSet.descriptors = prevDescriptors;
+	returnSet.feature2D = prevFeatures;
+	returnSet.feature3D = prevFeatures3D;
+	return returnSet;
 }
 
 
@@ -60,8 +69,10 @@ bool Matcher::match(const SensorFrame& sensorData, Eigen::Matrix4f &estimatedTra
 			showMatches(prevRgbImage, prevFeatures, sensorData.rgbImage, features, matches);
 
 	// RANSAC
+	// - neglect inlierMatches if you do not need them
+	std::vector<cv::DMatch> inlierMatches;
 	RANSAC ransac(matcherParameters.RANSACParams);
-	estimatedTransformation = ransac.estimateTransformation(prevFeatures3D, features3D, matches);
+	estimatedTransformation = ransac.estimateTransformation(prevFeatures3D, features3D, matches, inlierMatches);
 
 	// Save computed values for next iteration
 	features.swap(prevFeatures);
@@ -73,6 +84,60 @@ bool Matcher::match(const SensorFrame& sensorData, Eigen::Matrix4f &estimatedTra
 	prevDepthImage = sensorData.depthImage;
 
 	return false;
+}
+
+// We have chosen to use the first descriptor. TODO: Change it to be based on orientation
+cv::Mat Matcher::extractMapDescriptors(std::vector<MapFeature> mapFeatures) {
+	cv::Mat mapDescriptors;
+	for (std::vector<MapFeature>::iterator it = mapFeatures.begin();
+			it != mapFeatures.end(); ++it) {
+		mapDescriptors.push_back(it->descriptors[0].descriptor);
+	}
+	return mapDescriptors;
+}
+
+// Again, need to extract the Vec3 position of feature to reasonable format -> using nice std::algorithm
+std::vector<Eigen::Vector3f> Matcher::extractMapFeaturesPositions(std::vector<MapFeature> mapFeatures) {
+
+	std::vector<Eigen::Vector3f> mapFeaturePositions3D(mapFeatures.size());
+	std::transform(mapFeatures.begin(), mapFeatures.end(),
+	mapFeaturePositions3D.begin(),
+	[](const MapFeature& m) { return m.position.vector().cast<float>(); });
+	return mapFeaturePositions3D;
+	}
+
+bool Matcher::match(std::vector<MapFeature> mapFeatures,
+		std::vector<MapFeature> &foundInlierMapFeatures)
+{
+	// The current pose descriptors are renamed to make it less confusing
+	cv::Mat currentPoseDescriptors(prevDescriptors);
+
+	// We need to extract descriptors and positions from vector<class> to independent vectors to use OpenCV functions
+	cv::Mat mapDescriptors = extractMapDescriptors(mapFeatures);
+	std::vector<Eigen::Vector3f> mapFeaturePositions3D = extractMapFeaturesPositions(mapFeatures);
+
+	// Perform matching
+	std::vector<cv::DMatch> matches = performMatching(mapDescriptors,
+			prevDescriptors);
+
+	// RANSAC
+	std::vector<cv::DMatch> inlierMatches;
+	RANSAC ransac(matcherParameters.RANSACParams);
+	Eigen::Matrix4f estimatedTransformation = ransac.estimateTransformation(
+			mapFeaturePositions3D, prevFeatures3D, matches, inlierMatches);
+
+	// for all inliers, convert them to map-compatible format
+	foundInlierMapFeatures.clear();
+	for (std::vector<cv::DMatch>::iterator it = inlierMatches.begin(); it!=inlierMatches.end(); ++it) {
+		int mapId = it->queryIdx, currentPoseId = it->trainIdx;
+
+		MapFeature mapFeature;
+		mapFeature.id = mapId;
+		mapFeature.position = Vec3(prevFeatures3D[currentPoseId].cast<double>());
+		// TODO: take into account the future orientation
+		ExtendedDescriptor featureExtendedDescriptor(Quaternion::Identity(), prevDescriptors.row(currentPoseId) );
+		mapFeature.descriptors.push_back(featureExtendedDescriptor);
+	}
 }
 
 
