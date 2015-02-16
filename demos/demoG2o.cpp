aa6eb0e98e2e6f83f07c8ee4b232c1a3a6c0b8ac
dominikbelter <fffdd@fff.pl>
2015-02-16 19:27:46 +0100

    robust kernel off

--- demos/demoG2o.cpp
+++ demos/demoG2o.cpp
@@ -19,7 +19,8 @@ auto startT = std::chrono::high_resolution_clock::now();
 // optimization thread
 void optimize(){
     //optimize
-    graph->optimize(70);
+    graph->optimize(70,1);
+    //((PoseGraphG2O *)graph)->fixOptimizedVertices();
 }
 
 //simulates tracking module
@@ -29,25 +30,20 @@ void tracker()
     //add vertex or edge
 
     //add 3D feature
-    for (int i=0;i<5;i++){
-        Vec3 pos11(0.05, 1.0+i*0.05, 0.0);
-        Vertex3D vertex6(5+i, pos11);
+    for (int i=0;i<5000;i++){
+        Vertex3D vertex6(5+i, Vec3(0.05, 1.0+i*0.05, 0.0));
         vertex6.timestamp = duration_cast<std::chrono::microseconds> (high_resolution_clock::now() - startT).count();
         if (!graph->addVertexFeature(vertex6))
             std::cout << "error: vertex exists!\n";
     }
 
     //add edges of the graph -- measurements
-    Vec3 pos12(-1.05, 0.22, 0.33);
-    Mat33 infoMat12; infoMat12.setIdentity();
-    Edge3D edge6(pos12,infoMat12, 1,5);
+    Edge3D edge6(Vec3(-1.05, 0.22, 0.33), Mat33::Identity(), 1,5);
     if (!graph->addEdge3D(edge6))
         std::cout << "error: vertex doesn't exist!\n";
 
-    for (int i=0;i<5;i++){
-        Vec3 pos13(1.05+i*0.05, 0.0, 0.0);
-        Mat33 infoMat13; infoMat13.setIdentity();
-        Edge3D edge7(pos13,infoMat13, 2,5);
+    for (int i=0;i<5000;i++){
+        Edge3D edge7(Vec3(1.05+i*0.05, 0.0, 0.0), Mat33::Identity(), 2,5);
         if (!graph->addEdge3D(edge7))
             std::cout << "error: vertex doesn't exist!\n";
     }
@@ -67,73 +63,67 @@ int main()
         std::string configFile(config.FirstChildElement( "Grabber" )->FirstChildElement( "calibrationFile" )->GetText());
         DepthSensorModel sensorModel(configFile);
         graph = createPoseGraphG2O(sensorModel.config.pose);
+        graph->clear();
+        graph->load("../../resources/graphFileMN.g2o");
+        //graph->load("../../resources/KabschUncertainty/init_graph_g2o_BAuncert0.g2o");
+        optimize();
+        graph->save2file("../../resources/graphFileMN2.g2o");
+        getchar();
         cout << "Current graph: " << graph->getName() << std::endl;
 
         //add vertices - robot poses
-        Vec3 pos1(0.0, 0.0, 0.0);  Quaternion rot1(1, 0, 0, 0);
-        VertexSE3 vertex1(0, pos1, rot1);
+        VertexSE3 vertex1(0, Vec3(0.0, 0.0, 0.0), Quaternion (1, 0, 0, 0));
         vertex1.timestamp = duration_cast<std::chrono::microseconds> (high_resolution_clock::now() - startT).count();
         if (!graph->addVertexPose(vertex1))
             std::cout << "error: vertex exists!\n";
-        Vec3 pos2(0.0, 2.0, 0.0);  Quaternion rot2(1, 0, 0, 0);
-        VertexSE3 vertex2(1, pos2, rot2);
+
+        VertexSE3 vertex2(1, Vec3(0.0, 2.0, 0.0), Quaternion(1, 0, 0, 0));
         vertex2.timestamp = duration_cast<std::chrono::microseconds> (high_resolution_clock::now() - startT).count();
         if (!graph->addVertexPose(vertex2))
             std::cout << "error: vertex exists!\n";
-        Vec3 pos3(-2.0, 2.0, 0.0);  Quaternion rot3(1, 0, 0, 0);
-        VertexSE3 vertex3(2, pos3, rot3);
+
+        VertexSE3 vertex3(2, Vec3(-2.0, 2.0, 0.0), Quaternion(1, 0, 0, 0));
         vertex3.timestamp = duration_cast<std::chrono::microseconds> (high_resolution_clock::now() - startT).count();
         if (!graph->addVertexPose(vertex3))
             std::cout << "error: vertex exists!\n";
-        Vec3 pos4(-2.0, 0.0, 0.0);  Quaternion rot4(1, 0, 0, 0);
-        VertexSE3 vertex4(3, pos4, rot4);
+
+        VertexSE3 vertex4(3, Vec3(-2.0, 0.0, 0.0), Quaternion(1, 0, 0, 0));
         vertex4.timestamp = duration_cast<std::chrono::microseconds> (high_resolution_clock::now() - startT).count();
         if (!graph->addVertexPose(vertex4))
             std::cout << "error: vertex exists!\n";
 
         //add edges of the graph - eg. odometry measurements
-        Vec3 pos5(0.01, 1.06, 0.0); Quaternion rot5(0.999902, -0.0107791, 0.00867285, -0.00190021);
-        RobotPose trans1(pos5, rot5);
-        Mat66 infoMat5; infoMat5.setIdentity();
-        EdgeSE3 edge1(trans1,infoMat5,0,1);
+        EdgeSE3 edge1(RobotPose(Vec3(0.01, 1.06, 0.0), Quaternion(0.999902, -0.0107791, 0.00867285, -0.00190021)), Mat66::Identity(),0,1);
         if (!graph->addEdgeSE3(edge1))
             std::cout << "error: vertex doesn't exist!\n";
-        Vec3 pos6(-1.15, -0.09, 0.0); Quaternion rot6(0.999989, 0.00272799, -0.000777724, 0.00363979);
-        RobotPose trans2(pos6, rot6);
-        Mat66 infoMat6; infoMat6.setIdentity();
-        EdgeSE3 edge2(trans2,infoMat6,1,2);
+
+        EdgeSE3 edge2(RobotPose(Vec3(-1.15, -0.09, 0.0), Quaternion(0.999989, 0.00272799, -0.000777724, 0.00363979)), Mat66::Identity(),1,2);
         if (!graph->addEdgeSE3(edge2))
             std::cout << "error: vertex doesn't exist!\n";
-        Vec3 pos7(0.12, -1.02, 0.0); Quaternion rot7(0.99976, -0.00251893, -0.015912, 0.0148755);
-        RobotPose trans3(pos7, rot7);
-        Mat66 infoMat7; infoMat7.setIdentity();
-        EdgeSE3 edge3(trans3,infoMat7,2,3);
+
+        EdgeSE3 edge3(RobotPose (Vec3(0.12, -1.02, 0.0), Quaternion (0.99976, -0.00251893, -0.015912, 0.0148755)), Mat66::Identity(),2,3);
         if (!graph->addEdgeSE3(edge3))
             std::cout << "error: vertex doesn't exist!\n";
 
         //add 3D feature
-        Vec3 pos8(0.05, 1.0, 0.0);
-        Vertex3D vertex5(4, pos8);
+        Vertex3D vertex5(4, Vec3 (0.05, 1.0, 0.0));
         vertex5.timestamp = duration_cast<std::chrono::microseconds> (high_resolution_clock::now() - startT).count();
         if (!graph->addVertexFeature(vertex5))
             std::cout << "error: vertex exists!\n";
         //add edges of the graph -- measurements
         Mat33 infoMat9;        Eigen::Vector3d point;    Mat33 covMat9;
-        sensorModel.getPoint(320, 375, 5.1, point);
-        Vec3 pos9(point(0), point(1), point(2));
-        sensorModel.computeCov(320, 375, 5.1, covMat9);
+        sensorModel.getPoint(320, 375, 5.1, point);//get Euclidean coordinates
+
+        sensorModel.computeCov(320, 375, 5.1, covMat9);//compute covariance of measurement
         std::cout <<  infoMat9.inverse() << std::endl;
-        infoMat9 = covMat9.inverse();
-        Edge3D edge4(pos9,infoMat9, 0,4);
+        Edge3D edge4(Vec3(point(0), point(1), point(2)), covMat9.inverse(), 0,4);
         if (!graph->addEdge3D(edge4))
             std::cout << "error: vertex doesn't exist!\n";
 
         Mat33 infoMat10; infoMat10.setIdentity();     Mat33 covMat10;
         sensorModel.getPoint(320, 448, 3.16, point);
-        Vec3 pos10(point(0), point(1), point(2));
         sensorModel.computeCov(320, 448, 3.16, covMat10);
-        infoMat10 = covMat10.inverse();
-        Edge3D edge5(pos10,infoMat10, 1,4);
+        Edge3D edge5(Vec3 (point(0), point(1), point(2)), covMat10.inverse(), 1,4);
         if (!graph->addEdge3D(edge5))
             std::cout << "error: vertex doesn't exist!\n";
 
@@ -149,6 +139,11 @@ int main()
 
         tOpt.join();
         tTracker.join();
+
+
+        //optimize
+        std::thread tOpt2(optimize);
+        tOpt2.join();
 
         // graph pruning
         //graph->optimizeAndPrune(0.5, 10);
