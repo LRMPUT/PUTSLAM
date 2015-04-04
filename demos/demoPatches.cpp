da583b25a15e3740c8d6011ea38a54b38250735e
Michal Nowicki <michalsminowicki@gmail.com>
2015-04-04 23:37:19 +0200

    Reorganized patches - time to create a class out of it

--- demos/demoPatches.cpp
+++ demos/demoPatches.cpp
@@ -6,48 +6,38 @@
 #include <chrono>
 
 
-std::vector<uint8_t> computePatch (cv::Mat img, float x, float y, int patchSize) {
-	std::vector<uint8_t> patch;
+class MatchingOnPatches {
+	int verbose;
+	int patchSize;
 
-	// subpix precision
-	const float xLeft = int(x), yLeft = int(y);
-	const float xSub = x - xLeft, ySub = y - yLeft;
+	int maxIter;
+	float minSqrtIncrement;
 
-	// From wiki: http://upload.wikimedia.org/math/9/b/4/9b4e1064436ecccd069ea238b656c063.png
-	const float topLeft = (1.0 - xSub) * (1.0 - ySub);
-	const float topRight = xSub * (1.0 - ySub);
-	const float bottomLeft = (1.0 - xSub) * ySub;
-	const float bottomRight = xSub * ySub;
+public:
 
-	// compute patch
-	unsigned char *input = (unsigned char*) (img.data);
+	MatchingOnPatches(int _patchSize, int _maxIter = 20, float _minSqrtIncrement = 0.04, int _verbose = 0) {
+		patchSize = _patchSize;
+		verbose = _verbose;
 
-	// Speed-up considerations
-	const int halfPatchSize = (patchSize - 1) / 2;
-	const int yEnd = y + halfPatchSize;
-	const int xEnd = x + halfPatchSize;
-
-	for (int i = y - halfPatchSize; i <= yEnd; i++) {
-		for (int j = x - halfPatchSize; j <= xEnd;
-				j++) {
-
-			float value = topLeft * input[img.step * i + j]
-					+ topRight * input[img.step * (i) + j + 1]
-					+ bottomLeft * input[img.step * (i+1) + j]
-					+ bottomRight * input[img.step * (i+1) + j + 1];
-
-			patch.push_back(value);
-		}
+		maxIter = _maxIter;
+		minSqrtIncrement = _minSqrtIncrement;
 	}
 
-	return patch;
-}
 
+	std::vector<uint8_t> computePatch (cv::Mat img, float x, float y) {
+		std::vector<uint8_t> patch;
 
-void computeGradient(cv::Mat img, float x, float y, int patchSize,
-		Eigen::Matrix3f &InvHessian, std::vector<float> &gradientX,
-		std::vector<float> &gradientY) {
-	// compute patch
+		// subpix precision
+		const float xLeft = int(x), yLeft = int(y);
+		const float xSub = x - xLeft, ySub = y - yLeft;
+
+		// From wiki: http://upload.wikimedia.org/math/9/b/4/9b4e1064436ecccd069ea238b656c063.png
+		const float topLeft = (1.0 - xSub) * (1.0 - ySub);
+		const float topRight = xSub * (1.0 - ySub);
+		const float bottomLeft = (1.0 - xSub) * ySub;
+		const float bottomRight = xSub * ySub;
+
+		// compute patch
 		unsigned char *input = (unsigned char*) (img.data);
 
 		// Speed-up considerations
@@ -55,39 +45,109 @@ void computeGradient(cv::Mat img, float x, float y, int patchSize,
 		const int yEnd = y + halfPatchSize;
 		const int xEnd = x + halfPatchSize;
 
-		Eigen::Matrix3f Hessian = Eigen::Matrix3f::Zero();
-
 		for (int i = y - halfPatchSize; i <= yEnd; i++) {
-			for (int j = x - halfPatchSize; j <= xEnd; j++) {
+			for (int j = x - halfPatchSize; j <= xEnd;
+					j++) {
 
-				Eigen::Vector3f J;
-				J[0] = 0.5
-						* (input[img.step * i + j +1 ]
-								- input[img.step * i + j -1]);
-				J[1] = 0.5
-						* (input[img.step * (i+1) + j]
-								- input[img.step * (i-1) + j]);
-				J[2] = 1.0;
-				Hessian += J * J.transpose();
+				float value = topLeft * input[img.step * i + j]
+						+ topRight * input[img.step * (i) + j + 1]
+						+ bottomLeft * input[img.step * (i+1) + j]
+						+ bottomRight * input[img.step * (i+1) + j + 1];
 
-				gradientX.push_back(J[0]);
-				gradientY.push_back(J[1]);
+				patch.push_back(value);
 			}
 		}
-		InvHessian = Hessian.inverse();
-}
 
-inline void evaluatePatches(const std::vector<uint8_t> newPatch,
-		const std::vector<uint8_t> oldPatch, Eigen::Vector3f & tmpJ,
-		const std::vector<float> gradientX,
-		const std::vector<float> gradientY, const int patchSize) {
-	for (int i = 0; i < patchSize * patchSize; i++) {
-		float diff = newPatch[i] - oldPatch[i]; // + mean;
-		tmpJ[0] = tmpJ[0] - diff * gradientX[i];
-		tmpJ[1] = tmpJ[1] - diff * gradientY[i];
-		tmpJ[2] = tmpJ[2] - diff;
+		return patch;
 	}
-}
+
+	void computeGradient(cv::Mat img, float x, float y,	Eigen::Matrix3f &InvHessian, std::vector<float> &gradientX,
+			std::vector<float> &gradientY) {
+		// compute patch
+			unsigned char *input = (unsigned char*) (img.data);
+
+			// Speed-up considerations
+			const int halfPatchSize = (patchSize - 1) / 2;
+			const int yEnd = y + halfPatchSize;
+			const int xEnd = x + halfPatchSize;
+
+			Eigen::Matrix3f Hessian = Eigen::Matrix3f::Zero();
+
+			for (int i = y - halfPatchSize; i <= yEnd; i++) {
+				for (int j = x - halfPatchSize; j <= xEnd; j++) {
+
+					Eigen::Vector3f J;
+					J[0] = 0.5
+							* (input[img.step * i + j +1 ]
+									- input[img.step * i + j -1]);
+					J[1] = 0.5
+							* (input[img.step * (i+1) + j]
+									- input[img.step * (i-1) + j]);
+					J[2] = 1.0;
+					Hessian += J * J.transpose();
+
+					gradientX.push_back(J[0]);
+					gradientY.push_back(J[1]);
+				}
+			}
+			InvHessian = Hessian.inverse();
+	}
+
+	inline void evaluatePatches(const std::vector<uint8_t> newPatch,
+			const std::vector<uint8_t> oldPatch, Eigen::Vector3f & tmpJ,
+			const std::vector<float> gradientX,
+			const std::vector<float> gradientY) {
+		for (int i = 0; i < patchSize * patchSize; i++) {
+			float diff = newPatch[i] - oldPatch[i]; // + mean;
+			tmpJ[0] = tmpJ[0] - diff * gradientX[i];
+			tmpJ[1] = tmpJ[1] - diff * gradientY[i];
+			tmpJ[2] = tmpJ[2] - diff;
+		}
+	}
+
+	bool optimizeLocation(cv::Mat oldImg, std::vector<uint8_t> oldPatch,
+			cv::Mat newImg, float &newX, float &newY,
+			std::vector<float> gradientX, std::vector<float> gradientY,
+			Eigen::Matrix3f &InvHessian) {
+		// Jacobian and hessian of old patch
+
+
+			// Iterations of Gauss-Newton
+			float mean = 0;
+			for (int iter = 0; iter < maxIter ; iter++)
+			{
+				// Print some info
+				if (verbose > 0)
+					std::cout<<"ITER: " << iter << " | " << newX << " " << newY <<  " Mean: " << mean << std::endl;
+
+				// Compute newPatch
+				std::vector<uint8_t> newPatch = computePatch (oldImg, newX, newY);
+
+				// Evaluate patches
+				Eigen::Vector3f tmpJ = Eigen::Vector3f::Zero();
+				evaluatePatches(newPatch, oldPatch, tmpJ, gradientX, gradientY);
+
+				// Compute step
+				Eigen::Vector3f increment = InvHessian * tmpJ;
+				newX += increment[0];
+				newY += increment[1];
+				mean += increment[2];
+
+				// Ending condition
+				if (increment[0]*increment[0] + increment[1] * increment[1] < minSqrtIncrement) {
+					break;
+				}
+			}
+	}
+
+};
+
+
+
+
+
+
+
 
 int main()
 {
@@ -110,48 +170,27 @@ int main()
 	std::cout<<"x, y = " << x << " " << y << std::endl;
 	std::cout<<"newX, newY = " << newX << " " << newY <<std::endl;
 
-	// Patch size 9x9
-	const int patchSize = 9;
+
+
+	// Create matching on patches of size 9x9, verbose = 0
+	MatchingOnPatches matchingOnPatches(9);
 
 	// Compute old patch
 	std::vector<uint8_t> oldPatch;
-	oldPatch = computePatch(dst, x, y, patchSize);
+	oldPatch = matchingOnPatches.computePatch(dst, x, y);
 
-	// Jacobian and hessian of old patch
+	// Compute gradient
 	std::vector<float> gradientX, gradientY;
 	Eigen::Matrix3f InvHessian = Eigen::Matrix3f::Zero();
-	computeGradient(dst, x, y, patchSize, InvHessian, gradientX, gradientY);
+	matchingOnPatches.computeGradient(dst, x, y, InvHessian, gradientX, gradientY);
 
-	// Iterations of Gauss-Newton
-	float mean = 0;
-	int maxIter = 20;
-	const float minSqrtIncrement = 0.04;
-	for (int iter = 0; iter < maxIter ; iter++)
-	{
-		// Print some info
-		std::cout<<"ITER: " << iter << " | " << newX << " " << newY <<  " Mean: " << mean << std::endl;
 
-		// Compute newPatch
-		std::vector<uint8_t> newPatch = computePatch (dst, newX, newY, patchSize);
+	// Optimize position of the feature
+	matchingOnPatches.optimizeLocation(dst, oldPatch, dst, newX, newY, gradientX, gradientY, InvHessian);
 
-		// Evaluate patches
-		Eigen::Vector3f tmpJ = Eigen::Vector3f::Zero();
-		evaluatePatches(newPatch, oldPatch, tmpJ, gradientX, gradientY,
-				patchSize);
 
-		// Compute step
-		Eigen::Vector3f increment = InvHessian * tmpJ;
-		newX += increment[0];
-		newY += increment[1];
-		mean += increment[2];
-
-		// Ending condition
-		if (increment[0]*increment[0] + increment[1] * increment[1] < minSqrtIncrement) {
-			break;
-		}
-	}
 
 	// Print final result
-	std::cout<<std::endl<<"FINAL: " << newX << " " << newY <<  " Mean: " << mean << std::endl;
+	std::cout<<std::endl<<"FINAL: " << newX << " " << newY <<  std::endl;
 
 }
