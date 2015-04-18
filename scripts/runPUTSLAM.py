d750b6fb2c19eb47ee13cf4bdc01a8e28c531154
Michal Nowicki <michalsminowicki@gmail.com>
2015-04-18 19:53:11 +0200

    Corrections to runAll script

--- scripts/runPUTSLAM.py
+++ scripts/runPUTSLAM.py
@@ -7,15 +7,22 @@ import sys
 import glob
 import os
 
-# Path to save main results - create or clear
+# Path to save main results - create if needed
 if not os.path.exists("../../results"):
-	os.makedirs("../../results");
-else:
-	call('rm -rf ../../results/*', shell=True);	
+	os.makedirs("../../results");	
 
 # For all provided config files
 for root, dirs, files in os.walk("../../configs/"):
 	for dir in dirs:
+
+		# Create or clear a place to store those results
+		if not os.path.exists("../../results/"+dir):
+			os.makedirs("../../results/"+dir);
+		else:
+			print("Results exists - skip!");
+			continue;
+
+
 		# Copy to currently used settings
 		call('cp ' + root + dir+"/* ../../resources/", shell=True);	
 		
@@ -24,16 +31,14 @@ for root, dirs, files in os.walk("../../configs/"):
 
 		# Call ransac and map statistics 
 		call('python statistics.py', shell=True);
-		call('octave mapData.m > mapResults.res', shell=True);
+		call('octave mapData.m > mapResults.res', shell=True);		
 
-		# Create or clear a place to store those results
-		if not os.path.exists("../../results/"+dir):
-			os.makedirs("../../results/"+dir);
+		# Copy settings (create directory if needed)
+		if not os.path.exists("../../results/"+dir+"/resources"):
+			os.makedirs("../../results/"+dir+"/resources");
 		else:
-			call('rm -rf ../../results/' + dir + '/*', shell=True);
-
-		# Copy settings
-		call('cp ../../resources/* ../../results/' + dir + '/', shell=True);
+			call('rm -rf ../../results/' + dir + '/resources/*', shell=True);
+		call('cp ' + root + dir+"/* ../../results/" + dir + '/resources/', shell=True);
 
 		# Copy those results
 		call('mv *.png ../../results/' + dir + '/', shell=True);
